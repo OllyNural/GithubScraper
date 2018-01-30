e99872f1bed97903f4a40fab0c973d5d025e8777
Oliver Nural <onural@and.digital>
2018-01-30 09:38:07 +0000

    First working commit, need to update README will do later

---
+++ config-example.js
@@ -0,0 +1,19 @@
+const config = {
+    githubAuthenticateConfig: {
+        type: 'oauth',
+        token: ''
+    },
+    slackAuthenticationConfig: {
+        ClientID: '',
+        ClientSecret: '',
+        verificationToken: '',
+        webhookUri: '',
+        token: ''
+    },
+    slackConfig: {
+        channel: '',
+        username: ''
+    }
+}
+
+module.exports = config
