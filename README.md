9f86903ad9aee1d6122e0e80f8e72e23c6baaf4b
Oliver Nural <onural@and.digital>
2018-07-17 22:18:35 +0100

    tst commit

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # GithubScraper
-A small test script to scrape Github repos and send the oldies to Slack
+
+## Ideas
+Look at API, see if there's a way to view all files in a repo, or a pre-build spider for static analysis
+
+Scrape Github page file by file, and keep going down folders and building up a map locally
+
+Look into how Code Climate does it - they have an engine which this can potentially hook into: [Github repo here](https://github.com/codeclimate/spec/blob/master/SPEC.md)
--- index.js
+++ index.js
@@ -1,22 +1,30 @@
 const fetch = require('node-fetch')
-const GitHubApi = require('github')
+const octokit = require('@octokit/rest')()
 const Slack = require('slack')
 
 const config = require('./config.js')
 
-//https://www.npmjs.com/package/github
-const github = new GitHubApi()
-const slack = new Slack({token: config.slackAuthenticationConfig.token})
-
 async function start() {
-    // Authenticate
-    const authenticate = await github.authenticate(config.githubAuthenticateConfig)
-    // Get all the Repos belonging to ANDigital
-    const result = await github.repos.getForOrg({org: 'ANDigital', per_page: 100})
-    // Sort the results by date - oldest -> newest and keep oldest ones, add color
-    let repos = result.data.filter(repo => isDateBefore6Months(repo)).sort(sortByDates).map(repo => setFieldColor(repo))
-    // Send the filtered results to slack!
-    sendToSlack(repos)
+    const authenticate = await octokit.authenticate(config.githubAuthenticateConfig)
+
+    const owner = 'OllyNural'
+    const repo = 'GithubScraper'
+    const recursive = 1
+
+    let commits
+    try {
+        commits = await octokit.repos.getCommits({owner, repo})
+        let commit_sha = commits.data[0].sha
+        let commit = await octokit.gitdata.getCommit({owner, repo, commit_sha})
+        let tree_sha = commit.data.tree.sha
+        let tree = await octokit.gitdata.getTree({owner, repo, tree_sha, recursive})
+        console.log(tree)
+        console.log(tree.data.tree)
+
+    } catch (e) {
+        console.log("ERROR: ")
+        console.log(e)
+    }
 }
 
 const isDateBefore6Months = (repo) => {
@@ -33,52 +41,6 @@ const addYears = (date, years) => {
 
 const addMonths = (date, months) => {
     return date.setMonth(date.getMonth() + months)
-}
-
-// Get date diff and roughly do maths to get how old in months
-const setFieldColor = (repo) => {
-    let diff = (new Date().getTime() - new Date(repo.pushed_at).getTime()) / (1000 * 60 * 60 * 24 * 30)
-    if (diff > 24) {
-        repo.last_date_color = 'danger' 
-    } else if (diff > 18) {
-        repo.last_date_color = 'warning'
-    } else {
-        repo.last_date_color = 'good'
-    }
-    return repo
-}
-
-const formatMessages = (repos) => {
-    let reposToSend = {attachments: []}
-    for (let repo of repos) {
-        reposToSend.attachments.push({
-            title: repo.full_name,
-            title_link: repo.html_url,
-            color: repo.last_date_color,
-            fields: [
-                {
-                    title: 'Last pushed',
-                    value: new Date(repo.pushed_at).toDateString(),
-                    short: true
-                }
-            ]
-        })
-    }
-    return reposToSend
-}
-
-async function sendToSlack(repos) {
-    // True to send a pm to the username in config
-    // False to send a message to the channel in config
-    let sendPM = false
-    // Send IM instead of channel
-    let users = await slack.users.list()
-    let theUser = users.members.filter(user => user.name === config.slackAuthenticationConfig.channel)[0]
-
-    const placeToSend = sendPM ? theUser.id : config.slackConfig.channel
-    
-    let formattedMessages = formatMessages(repos)
-    slack.chat.postMessage({text: 'List of recently unused github projects', attachments: reposToSend.attachments, channel: placeToSend, username: 'Github bot'})
 }
 
 start()
